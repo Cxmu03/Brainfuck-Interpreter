53552b69a27bca77ac37429b708b9dcd11238d0d
Marek Freunscht <Freunscht58@outlook.de>
2021-03-18 18:38:40 +0100

    Code refactoring

--- Brainfuck Interpreter/src/Interpreter.cs
+++ Brainfuck Interpreter/src/Interpreter.cs
@@ -28,6 +28,8 @@ namespace Brainfuck_Interpreter
 		//Keeps track of all indices of loop begins for nested loops
 		private Stack<int> loopBeginPointers;
 
+		private char[] tokens;
+
 		public Interpreter()
 		{
 			Reset();
@@ -35,83 +37,93 @@ namespace Brainfuck_Interpreter
 
 		public void InterpretCode()
 		{
-			var tokens = Instructions.Where(x => brainfuckInstructions.Contains(x)).ToArray();
+			tokens = Instructions.Where(x => brainfuckInstructions.Contains(x)).ToArray();
 
 			while(instructionPointer < tokens.Count())
 			{
-				switch(tokens[instructionPointer])
-				{
-					case '>':
-						memoryPointer++;
-						break;
-					case '<':
-						memoryPointer--;
-						break;
-					case '+':
-						memory[memoryPointer]++;
-						break;
-					case '-':
-						memory[memoryPointer]--;
-						break;
-					case '.':
-						//Brainfuck standard to use 10 for a newline
-						if (memory[memoryPointer] == 10)
-							outputBuffer.Append('\n');
-						else
-							outputBuffer.Append((char) memory[memoryPointer]);
-						break;
-					case ',':
-						Console.Write("Enter a character: ");
-						char c = Console.ReadKey().KeyChar;
-						memory[memoryPointer] = (byte) c;
-
-						//Entering a newline after receiving character
-						Console.Write("\n\n");
-						break;
-					case '[':
-						//If current cell is 0 then jump behind the corresponding closing bracket
-						if(memory[memoryPointer] == 0)
-						{
-							//0 based loop depth
-							int depth = 0;
-							instructionPointer++;
-							while (instructionPointer < tokens.Count())
-							{
-								//New loop is one depth deeper
-								if (tokens[instructionPointer] == '[')
-									depth++;
-								//Corresponding bracket found
-								else if (tokens[instructionPointer] == ']' && depth == 0)
-									break;
-								//Inner closing bracket found
-								else
-									depth--;
-							}
-						}
-						else
-						{
-							//Add loop begin index to stack
-							loopBeginPointers.Push(instructionPointer);
-						}
-						break;
-					case ']':
-						if(memory[memoryPointer] == 0)
-						{
-							//Remove innermost loop begin pointer
-							loopBeginPointers.Pop();
-						}
-						else
-						{
-							//Get innermost loop begin pointer and jump there
-							instructionPointer = loopBeginPointers.Peek();
-						}
-						break;
-				}
-
-				instructionPointer++;
+				NextStep();
 			}
 
 			Console.WriteLine($"Output:\n{outputBuffer.ToString()}\n");
+		}
+
+		private void NextStep()
+		{
+			switch (tokens[instructionPointer])
+			{
+				case '>':
+					memoryPointer++;
+					break;
+				case '<':
+					memoryPointer--;
+					break;
+				case '+':
+					memory[memoryPointer]++;
+					break;
+				case '-':
+					memory[memoryPointer]--;
+					break;
+				case '.':
+					//Brainfuck standard to use 10 for a newline
+					if (memory[memoryPointer] == 10)
+						outputBuffer.Append('\n');
+					else
+						outputBuffer.Append((char)memory[memoryPointer]);
+					break;
+				case ',':
+					Console.Write("Enter a character: ");
+					char c = Console.ReadKey().KeyChar;
+					memory[memoryPointer] = (byte)c;
+
+					//Entering a newline after receiving character
+					Console.Write("\n\n");
+					break;
+				case '[':
+					//If current cell is 0 then jump behind the corresponding closing bracket
+					if (memory[memoryPointer] == 0)
+					{
+						JumpToMatchingClosingBracket();
+					}
+					else
+					{
+						//Add loop begin index to stack
+						loopBeginPointers.Push(instructionPointer);
+					}
+					break;
+				case ']':
+					if (memory[memoryPointer] == 0)
+					{
+						//Remove innermost loop begin pointer
+						loopBeginPointers.Pop();
+					}
+					else
+					{
+						//Get innermost loop begin pointer and jump there
+						instructionPointer = loopBeginPointers.Peek();
+					}
+					break;
+			}
+
+			instructionPointer++;
+		}
+
+		private void JumpToMatchingClosingBracket()
+		{
+			//0 based loop depth
+			int depth = 0;
+			instructionPointer++;
+			while (instructionPointer < tokens.Count())
+			{
+				//New loop is one depth deeper
+				if (tokens[instructionPointer] == '[')
+					depth++;
+				//Corresponding bracket found
+				else if (tokens[instructionPointer] == ']' && depth == 0)
+					break;
+				//Inner closing bracket found
+				else
+					depth--;
+			}
 		}
 
 		public void Reset()
--- Brainfuck Interpreter/src/Program.cs
+++ Brainfuck Interpreter/src/Program.cs
@@ -23,48 +23,53 @@ namespace Brainfuck_Interpreter
 					args = new string[0];
 				}
 				else
-				{
-					Console.Clear();
-					Console.Write("1. By file\n2. By Brainfuck code\nChoice: ");
-					var choice = Console.ReadKey().KeyChar;
-					switch(choice)
-					{
-						case '1':
-							string inputFilename = string.Empty;
-							do
-							{
-								Console.Clear();
-								Console.Write("Enter filename: ");
-								inputFilename = Console.ReadLine();
-							} while (!File.Exists(inputFilename));
-
-							code = File.ReadAllText(inputFilename);
-
-							Console.Write("\n");
-							break;
-						default:
-							Console.Clear();
-							Console.WriteLine("Enter your brainfuck code:");
-							string inputString = string.Empty;
-							do
-							{
-								inputString = Console.ReadLine();
-								code += inputString;
-							} while (!(string.IsNullOrEmpty(inputString)));
-							break;
-					}
-				}
+					code = GetInput();
 
 				brainfuckInterpreter.Instructions = code;
-
 				brainfuckInterpreter.InterpretCode();
-
 				brainfuckInterpreter.Reset();
 
 				Console.WriteLine("Press any key to continue...");
 				//New iteration on keypress
 				Console.ReadKey();
 			}
+		}
+
+		private static string GetInput()
+		{
+			string code = string.Empty;
+
+			Console.Clear();
+			Console.Write("1. By file\n2. By Brainfuck code\nChoice: ");
+			var choice = Console.ReadKey().KeyChar;
+			switch (choice)
+			{
+				case '1':
+					string inputFilename = string.Empty;
+					do
+					{
+						Console.Clear();
+						Console.Write("Enter filename: ");
+						inputFilename = Console.ReadLine();
+					} while (!File.Exists(inputFilename));
+
+					code = File.ReadAllText(inputFilename);
+
+					Console.Write("\n");
+					break;
+				default:
+					Console.Clear();
+					Console.WriteLine("Enter your brainfuck code:");
+					string inputString = string.Empty;
+					do
+					{
+						inputString = Console.ReadLine();
+						code += inputString;
+					} while (!(string.IsNullOrEmpty(inputString)));
+					break;
+			}
+
+			return code;
 		}
 	}
 }
